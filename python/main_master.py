8283356e8a4cdce57b6e9f48d41fb7a81b2a7b56
shak <sovannara.hak@isir.upmc.fr>
2012-12-06 09:54:30 +0100

    Fix missing dynamic model construction

--- python/main_master.py
+++ python/main_master.py
@@ -27,6 +27,8 @@ sys.path.append(cpath)
 import rtt_interface_corba
 rtt_interface_corba.Init(sys.argv)
 
+import kb
+
 TIME_STEP = .01
 print "TIME_STEP ", TIME_STEP
 
@@ -45,6 +47,7 @@ import scene1
 mechaName = "kuka1"
 
 world = scene1.buildKuka(mechaName)
+worldKuka1 = scene1.buildKuka(mechaName)
 scene1.addContactLaws(world)
 scene1.addGround(world)
 scene1.addWall(world)
@@ -66,6 +69,7 @@ import physic
 
 phy = physic.createTask()
 physic.init(TIME_STEP)
+physic.createDynamicModel(worldKuka1, mechaName)
 physic.deserializeWorld(world, mechaName)
 
 print "CREATE PORTS..."
@@ -90,7 +94,7 @@ rtt_interface_corba.SetServer(qreader._obj)
 print "CREATE CONTROLLER..."
 #####
 # add orcisir_ISIRController
-oIT = ddeployer.load("oIT", "Orocos_ISIRController", module="orcisir_Orocos_IsirController-gnulinux", prefix="", libdir="/home/shak/src/orcisir_IsirController/_build/src/")
+oIT = ddeployer.load("oIT", "Orocos_ISIRController", module="orcisir_Orocos_IsirController-gnulinux", prefix="", libdir="/home/shak/compil/samaxe-64/lib/orocos/gnulinux/orcisir_IsirController/")
 TT = dsimi.rtt.Task(oIT)
 
 phy.getPort(mechaName+"_q").connectTo(TT.getPort("q"))
@@ -116,17 +120,17 @@ qDes = 0.*ones(dm.nbDofs())
 dqDes = zeros(dm.nbDofs())
 pqDes = -0.7*ones(2)
 pdqDes = zeros(2)
-posDes = lgsm.Displacement(.3, .3, .3)
+posDes = lgsm.Displacement(.38, .110, .30)
 velDes = lgsm.Twist()
-TT.s.updateTasks("createTask  taskFull  4  1  fullState")
-TT.s.updateTasks("updateTask  taskFull  -Kp {0}  -Kd {1}  -qDes {2}  -dqDes {3}  endUpdate".format(Kp, Kd, lToS(qDes), lToS(dqDes)))
-TT.s.updateTasks("createTask  taskPart  3  1  partialState 2 0 1")
-TT.s.updateTasks("updateTask  taskPart  -Kp {0}  -Kd {1}  -partqDes {2}  -partdqDes {3}  endUpdate".format(Kp, Kd, lToS(pqDes), lToS(pdqDes)))
+#TT.s.updateTasks("createTask  taskFull  4  1  fullState")
+#TT.s.updateTasks("updateTask  taskFull  -Kp {0}  -Kd {1}  -qDes {2}  -dqDes {3}  endUpdate".format(Kp, Kd, lToS(qDes), lToS(dqDes)))
+#TT.s.updateTasks("createTask  taskPart  3  1  partialState 2 0 1")
+#TT.s.updateTasks("updateTask  taskPart  -Kp {0}  -Kd {1}  -partqDes {2}  -partdqDes {3}  endUpdate".format(Kp, Kd, lToS(pqDes), lToS(pdqDes)))
 #TT.s.updateTasks("createTask  taskFrameOri  2  1  frame 07 {0} R".format(lToS(lgsm.Displacement())))
 #TT.s.updateTasks("updateTask  taskFrameOri  -Kp {0}  -Kd {1}  -posDes {2}  -velDes {3}  endUpdate".format(Kp, Kd, lToS(posDes), lToS(velDes)))
 
-#TT.s.updateTasks("createTask  taskFrame  1  1  frame 07 {0} XYZ".format(lToS(lgsm.Displacement())))
-#TT.s.updateTasks("updateTask  taskFrame  -Kp {0}  -Kd {1}  -posDes {2}  -velDes {3}  endUpdate".format(Kp, Kd, lToS(posDes), lToS(velDes)))
+TT.s.updateTasks("createTask  taskFrame  1  1  frame 07 {0} XYZ".format(lToS(lgsm.Displacement())))
+TT.s.updateTasks("updateTask  taskFrame  -Kp {0}  -Kd {1}  -posDes {2}  -velDes {3}  endUpdate".format(Kp, Kd, lToS(posDes), lToS(velDes)))
 
 
 #################################################
@@ -136,8 +140,12 @@ dm.setJointPositions(np.array([.4]*7).reshape(7,1))
 robot.setJointVelocities(np.array([0.0]*7).reshape(7,1))
 dm.setJointVelocities(np.array([0.0]*7).reshape(7,1))
 
+graphic.graph_scn.MarkersInterface.addMarker("target", False)
+graphic.graph_scn.MarkersInterface.setMarker6DPosition("target", posDes)
+
 #Enable contacts
 physic.robot.enableContactWithBody("ground", True)
+physic.robot.enableContactWithBody("env1", True)
 
 phy.s.setPeriod(TIME_STEP)
 TT.s.setPeriod(TIME_STEP)
@@ -145,6 +153,16 @@ qreader.s.setPeriod(TIME_STEP)
 graph.s.start()
 phy.s.start()
 clock.s.start()
+
+# Temp Keyboard manager
+kmf = dsimi.rtt.Task(graph.s.Dio.getKeyboardFactory())
+km = dsimi.rtt.Task(kmf.s.build('decoy', 'keyb'))
+graph.getPort('out_kmEvent').connectTo(km.getPort('in_kmEvent'))
+kbi = kb.KeyboardInterface("kbi", TIME_STEP, posDes, TT, km.getPort("out_kmEvent"), Kp, Kd, velDes, "target", graphic.graph_scn)
+kbi.s.start()
+km.s.start()
+
+####
 
 def start():
     global TT
@@ -167,8 +185,6 @@ def stop():
 
     print "07 pos\n", array(robot.getSegmentPosition2("07")[0:3])
     print "07 ori\n", array(robot.getSegmentPosition2("07")[3:7])
-
-
 
 from time import sleep
 sleep(.1)
